a1fde6347ad8a773bc33f780e74cabf2d3a99e8e
sebastianj7 <sebastianjacobs2000@gmail.com>
2024-07-15 18:17:11 +0200

    feat: :sparkles: select location
    provided a list of major cities around the world that user can click on to geth the forecast for that city
    
    none

--- weather-app-project/main.ts
+++ weather-app-project/main.ts
@@ -1,3 +1,5 @@
 import { fetchWeatherData } from "./open-meteo.ts";
+import { initializeCityDivs } from "./locations.ts";
 
-fetchWeatherData();
+fetchWeatherData(-26.2023, 28.0436);
+initializeCityDivs();
